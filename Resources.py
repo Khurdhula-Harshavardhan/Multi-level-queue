3f096e8986365f4d812eb3d04832c8d4ae490d08
Harsha <khurdhulaharshavardhan@gmail.com>
2023-03-30 19:33:24 -0400

    adds attributes to Queue.

--- Resources.py
+++ Resources.py
@@ -4,14 +4,20 @@ that might be needed inorder to implement a multi level feedback queue.
 """
 
 class Queue():
+    __NAME = None
     __front = None
     __rear = None
+    __TIME_QUANTUM = None
+    __waiting = list()
+
     
-    def __init__(self) -> None:
+    def __init__(self, name, time_quantum) -> None:
         """
         This is the constructor that initializes the Queue pointers.
         """
         try:
-            pass
+            #lets define the time_quantum for the queue.
+            self.__TIME_QUANTUM = time_quantum
+            self.__NAME = name
         except Exception as e:
             print("[ERR] The following error occured while trying to initialize a new Queue: "+str(e))
