3aece18c91a0ea81311ac2eb28eb10336114d4e5
Harsha <khurdhulaharshavardhan@gmail.com>
2023-03-30 19:23:11 -0400

    Defines Queue attributes.

--- Resources.py
+++ Resources.py
@@ -4,4 +4,14 @@ that might be needed inorder to implement a multi level feedback queue.
 """
 
 class Queue():
-    pass
+    __front = None
+    __rear = None
+    
+    def __init__(self) -> None:
+        """
+        This is the constructor that initializes the Queue pointers.
+        """
+        try:
+            pass
+        except Exception as e:
+            print("[ERR] The following error occured while trying to initialize a new Queue: "+str(e))
