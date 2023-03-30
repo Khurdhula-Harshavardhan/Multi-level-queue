66a1a5ea48f93854a43f7b2a754b915059702350
Harsha <khurdhulaharshavardhan@gmail.com>
2023-03-30 19:40:59 -0400

    Refractors code to create individual queues.

--- Resources.py
+++ Resources.py
@@ -3,7 +3,7 @@ This python script acts as a resource for all datastructures and entities
 that might be needed inorder to implement a multi level feedback queue.
 """
 
-class Queue():
+class Queue_A():
     __NAME = None
     __front = None
     __rear = None
@@ -11,13 +11,24 @@ class Queue():
     __waiting = list()
 
     
-    def __init__(self, name, time_quantum) -> None:
+    def __init__(self) -> None:
         """
         This is the constructor that initializes the Queue pointers.
         """
         try:
             #lets define the time_quantum for the queue.
-            self.__TIME_QUANTUM = time_quantum
-            self.__NAME = name
+            self.__TIME_QUANTUM = 5
+            self.__NAME = "A"
         except Exception as e:
             print("[ERR] The following error occured while trying to initialize a new Queue: "+str(e))
+
+    def is_empty(self) -> bool:
+        """
+        Returns a boolean value, 
+        True if there are no elements within the list waiting.
+        False, otherwise.
+        """
+        if len(self.__waiting) == 0:
+            return True
+        else:
+            return False
