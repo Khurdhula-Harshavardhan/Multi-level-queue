532e266e5c68b95d1e35b0fbc2b4f2464240eff7
Harsha <khurdhulaharshavardhan@gmail.com>
2023-03-31 16:35:45 -0400

    accepts user input.

--- Simulation.py
+++ Simulation.py
@@ -9,11 +9,30 @@ from Console import Console
 
 
 class Simulation():
+    __file_handler = None
+    __dispatch_ratio = None
+    __demotion_criteria = None
+    __data = None
     def __init__(self) -> None:
         self.console = Console()
         self.cpu = CPU(self.console)
         self.qa = Queue_A(self.console)
-        
+    
+    def convert_data(self) -> None:
+        """
+        Converts the input file text, into useful readable format.
+        """
+        try:
+            self.data = self.data.split("\n")
+            data = list()
+            for value in self.data:
+                try:
+                    data.append(int(value))
+                except:
+                    data.append(-1)
+            self.data = data
+        except Exception as e:
+            print("[ERR] The following error occured while trying to read job file: "+str(e))
 
         
 
@@ -22,6 +41,25 @@ class Simulation():
         Starting point of the Simulation for this Multi level queue.
         """
         try:
+
+            #Get user inputs.
+            self.console.note_activity("[I/O] Asking user for job file name.")
+            file_name = "TestCase.txt" #input("Please enter the job file name: ")
+            self.console.note_activity("[I/O] User entered the following string for job file name: " +file_name)
+            self.console.note_activity("[I/O] Asking user for Dispatch Ratio.")
+            self.__dispatch_ratio = 4 #int(input("Please enter dispatch ratio: "))
+            self.console.note_activity("[I/O] User entered the following value for Dispatch Ratio: "+str(self.__dispatch_ratio))
+            self.console.note_activity("[I/O] Asking user for Demotion Criteria.")
+            self.__demotion_criteria = 5 #int(input("Please enter demotion criteria: "))
+            self.console.note_activity("[I/O] User entered the following value for Demotion Criteria: "+str(self.__demotion_criteria))
+            self.__file_handler = open(file_name, "r", encoding="UTF-8")
+            self.data = self.__file_handler.read()
+            self.console.note_activity("[I/O] Reading file: " +file_name)
+            self.convert_data()
+            self.console.note_activity("[I/O] Converting data within file:" +file_name)
+            print(self.data)
+
+            #Begin Execution.
             for i in range(10):
                 self.cpu.update_clock_cycle(i)
                 process = Process(name= "P"+str(i+1), burst_time=i+1, console=self.console)
