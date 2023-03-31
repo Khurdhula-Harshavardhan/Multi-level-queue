c235c0d6ca9f44f8d3fabe64e5a0d07299c6e8f8
Harsha <khurdhulaharshavardhan@gmail.com>
2023-03-31 13:30:54 -0400

    adds new module Console. That will document simulation activities.

---
+++ Console.py
@@ -0,0 +1,8 @@
+"""
+I find the need to see what's happening in the backend while this simulation is running:
+Hence I'd like to document each activity of the Simulation.
+In-order to achieve this, I shall write a new Component/Class that will implement this idea of documentation: Console.
+"""
+
+class Console():
+    pass
